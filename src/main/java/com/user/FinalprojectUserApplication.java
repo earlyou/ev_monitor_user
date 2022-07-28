1c80c4aa5bec9a66318af117813a946f2771298c
earlyou <earlyou1@gmail.com>
2022-07-28 18:09:39 +0900

    first commit

---
+++ src/main/java/com/user/FinalprojectUserApplication.java
@@ -0,0 +1,13 @@
+package com.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalprojectUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalprojectUserApplication.class, args);
+	}
+
+}
